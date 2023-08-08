38efcb58bdbf1cdeb4ea68adc1f098a058b984a1
HaraTakae <140220718+HaraTakae@users.noreply.github.com>
2023-08-08 16:41:34 +0000

    Add

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,14 +5,14 @@ class ListsController < ApplicationController
   end
 
   # 以下を追加
-  def create
-    # 1 .&2. データを受け取り新規登録するためのインスタンス作成
-    list = List.new(list_params)
-    # 3. データをデータベースに保存するためのsaveメゾット実行
-    list.save
-    # 4. トップ画面へリダイレクト
-    redirect_to list_path(list.id)
+def create
+  @list = List.new(list_params)
+  if @list.save
+    redirect_to list_path(@list.id)
+  else
+    render :new
   end
+end
 
   def index
     @lists = List.all
@@ -31,7 +31,7 @@ class ListsController < ApplicationController
     list.update(list_params)
     redirect_to list_path(list.id)
   end
-  
+
   def destroy
     list = List.find(params[:id]) # データ（レコード）を1件取得
     list.destroy # データ（レコード）を削除
